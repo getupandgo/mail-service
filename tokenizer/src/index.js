50e77ff2ae791ee395b8c2ba549c99e283279540
getupandgo <getupandgo11@gmail.com>
2017-05-28 14:37:34 +0300

    queries for getting and updating tokens

--- tokenizer/src/index.js
+++ tokenizer/src/index.js
@@ -1,7 +1,49 @@
+const countBy = require('lodash.countby')
+const differenceWith = require('lodash.differencewith')
+const uniq = require('lodash.uniq')
+
+const Token = require('../models').Token
+
 module.exports.updateTokenFrequency = function (body) {
-  const tokens = body.match(/[a-z'\-]+/gi)
+  let splittedText = body.match(/[a-z'\-]+/gi)
+  const tokenFrequency = countBy(splittedText)
+
+  splittedText = uniq(splittedText, 'token')
+
+  return Token.findAll({
+    attributes: ['token', 'count'],
+    where: {
+      token: {
+        $in: splittedText
+      }
+    }
+  })
+  .then(tokensToUpdate => {
+    const updatePromise = Promise.all(
+      tokensToUpdate.map(token => {
+        return token.update({
+            count: token.count + tokenFrequency[token.token]
+        })
+      })
+    )
+
+    const newTokens = differenceWith(splittedText, tokensToUpdate, (textTokenVal, tokenToUpdateVal) => {
+      return textTokenVal === tokenToUpdateVal.token
+    })
+
+    const newRecords = newTokens.map(token => {
+      return {token, count: tokenFrequency[token]}
+    })
+
+    const createPromise = Token.bulkCreate(newRecords)
+
+    return Promise.all([updatePromise, createPromise])
+  })
 }
 
-module.exports.getCommonTokens = function (body) {
-
+module.exports.getCommonTokens = function () {
+  return Token.findAll({
+    order: 'count',
+    limit: 10
+  })
 }
