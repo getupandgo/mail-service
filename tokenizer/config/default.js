df934e95c4ab9a869b600f469adc96dfb6a42e94
getupandgo <getupandgo11@gmail.com>
2017-05-29 13:32:33 +0300

    db moved to elephantsql

--- tokenizer/config/default.js
+++ tokenizer/config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: 3001,
   db: {
-    url: 'postgres://mvoocuvk:c9V3baQICVra1lYT8cOFxsWzk3QiQ0u9@stampy.db.elephantsql.com:5432/mvoocuvk',
+    url: 'postgres://mvoocuvk:c9V3baQICVra1lYT8cOFxsWzk3QiQ0u9@stampy.db.elephantsql.com:5432/mvoocuvk'
   },
   amqp: 'amqp://localhost'
 }
