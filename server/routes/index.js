8f34a5a4ab411d704352844c3c2d8528c5c6329e
getupandgo <getupandgo11@gmail.com>
2017-05-29 13:45:33 +0300

    separate method for consume message from channel

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,23 +3,31 @@ const express = require('express')
 module.exports = function (channel) {
   const router = express.Router()
 
-  router.get('/tokens', (req, res) => {
+  router.get('/tokens', async (req, res) => {
     channel.sendToQueue('req_tokens', Buffer.from('send'))
 
-    channel.consume('res_tokens', msg => {
-      if (msg) {
-        res.send(msg.content.toString())
-        channel.ack(msg)
-      }
-    })
+    const content = await getResponse(channel)
+    res.send(content)
   })
 
   router.post('/message', (req, res) => {
     channel.sendToQueue('send_email', req.body)
     channel.sendToQueue('update_tokens', req.body)
 
-    res.sendStatus(200)
+    return res.sendStatus(200)
   })
 
   return router
+}
+
+const getResponse = function (channel) {
+  return new Promise((resolve, reject) => {
+      channel.consume('res_tokens', msg => {
+          if (msg) {
+              const content = msg.content.toString()
+              channel.ack(msg)
+              resolve(content)
+          } else { reject() }
+      })
+  })
 }
